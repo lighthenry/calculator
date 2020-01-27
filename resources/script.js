965c05f58221b4e2eb84de385d8ef4d6aa196194
lighthenry <daniel.holman99@gmail.com>
2020-01-27 13:31:23 +0000

    Screen added

--- resources/script.js
+++ resources/script.js
@@ -17,3 +17,59 @@ function divide(x, y) {
 function multiply(x, y) {
     return x * y;
 }
+
+function operate(op, x, y) {
+    switch (op) {
+        case "+": 
+            return add(x, y);  
+        case "-": 
+            return subtract(x, y); 
+        case "x": 
+            return multiply(x, y); 
+        case "/": 
+            return divide(x, y);  
+    }
+}
+
+let firstNumber = 0; 
+let secondNumber = 0; 
+let operator = "";
+var breakException = "";  
+let num="";
+let num1 = 0; let num2 = 0;
+
+
+let screen = document.querySelector(".screen");
+let buttons = document.querySelectorAll(".buttons"); 
+
+function Nums() {
+    buttons.forEach(button => {
+        button.addEventListener("mousedown", event => {
+            let num = event.target.value;
+            firstNumber = `${screen.innerHTML}` + `${num}`;
+            screen.innerHTML = firstNumber;
+            if (num == "+" || num == "-" || num == "x" || num == "/") {
+                num1 = firstNumber.substring(0, firstNumber.length-1);
+                operator = firstNumber.charAt(firstNumber.length - 1);
+            }
+            console.log(num1, operator);
+            
+        });
+    });
+};
+
+
+
+function clear() {
+    let clrButton = document.querySelector("#clr"); 
+    clrButton.addEventListener("mousedown", event => {
+        screen.innerHTML = event.target.value;
+    });
+}
+
+
+
+
+
+Nums(); 
+clear();
